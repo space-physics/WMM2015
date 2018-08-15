3689445b952e05c6c8bbdf72288e6c4fca145498
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-14 23:43:03 -0400

    windows setup cmake

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 import subprocess
 from setuptools import setup
+import os
 
 setup()
 
-subprocess.check_call(['cmake', '../src'], cwd='bin')
+if os.name == 'nt':
+    subprocess.check_call(['cmake', '-G', 'MinGW Makefiles',
+                           '-DCMAKE_SH="CMAKE_SH-NOTFOUND', '../src'], cwd='bin')
+else:
+    subprocess.check_call(['cmake', '../src'], cwd='bin')
+
 subprocess.check_call(['cmake', '--build', '.'], cwd='bin')
